382ab1f02a41ed8cc48c7cd1fea3c7f2ee5881b0
Maksim Kononykhin <kononelder@gmail.com>
2024-03-08 15:14:36 +0300

    WIP

---
+++ MLCore/ML_dispatcher.py
@@ -0,0 +1,26 @@
+import pika
+from pymongo import MongoClient
+
+
+class MLDispatcher:
+    def __init__(self, ip: str = 'localhost', port:int = 1338):
+        self.ip = ip
+        self.port = port
+
+    def __callback(self, ch, method, properties, body):
+            print(body.decode())
+
+    def run(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(self.ip, self.port,\
+            virtual_host='/',credentials=pika.PlainCredentials('guest', 'guest'), heartbeat=600, blocked_connection_timeout=300))
+        channel = connection.channel()
+
+        queue_name = 'transmit_queue'
+
+        channel.basic_consume(queue=queue_name, on_message_callback=self.__callback, auto_ack=True)
+
+        channel.start_consuming()
+
+
+dispatcher = MLDispatcher()
+dispatcher.run()
--- MLCore/__main__.py
+++ MLCore/__main__.py
@@ -1,2 +1,6 @@
+from argparse import ArgumentParser
+from ML_dispatcher import MLDispatcher
+
+
 if __name__ == '__main__':
     pass
