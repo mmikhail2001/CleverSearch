9336449fc56f9521e605da0b4a3cf8b9da61cfb6
Maksim Kononykhin <kononelder@gmail.com>
2024-03-09 17:45:46 +0300

    stage commit

--- MLCore/__main__.py
+++ MLCore/__main__.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser
 from ML_dispatcher import MLDispatcher
 
+arg_parser = ArgumentParser('MLCore', description='heart of ML in CleverSearch')
+
+
 
 if __name__ == '__main__':
     pass
