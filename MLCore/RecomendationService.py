d5c4a9094a0586c9272ccdd7cf1ac603b48d050d
Anastasia Dolotova <ad4019809@gmail.com>
2024-03-09 21:10:29 +0300

    ML-CLEVER-1: delete unnecessary import

--- MLCore/RecomendationService.py
+++ MLCore/RecomendationService.py
@@ -1,6 +1,5 @@
 import numpy as np
 from pandas import DataFrame
-from pymongo import MongoClient
 from pymongo.collection import Collection
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
