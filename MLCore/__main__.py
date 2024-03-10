4c76e87e64d9a7f94f7ae391be35e6c90e05bc09
Anastasia Dolotova <ad4019809@gmail.com>
2024-03-10 17:35:19 +0300

    ML-CLEVER-1: flake8 & pylint

--- MLCore/__main__.py
+++ MLCore/__main__.py
@@ -1,16 +1,25 @@
-from argparse import ArgumentParser
-from ML_dispatcher import MLDispatcher
-import uvicorn
 import sys
-sys.path.insert(1, './MLCore/')
-sys.path.insert(2, './MLCore/Services')
+from argparse import ArgumentParser
+
+import uvicorn
+from ML_dispatcher import MLDispatcher
+
 from Services.ImageService import ImageService
 from Services.RecomendationService import RecomendationService, app
+sys.path.insert(1, './MLCore/')
+sys.path.insert(2, './MLCore/Services')
 
-arg_parser = ArgumentParser('MLCore', description='heart of ML in CleverSearch')
+arg_parser = ArgumentParser(
+        'MLCore',
+        description='heart of ML in CleverSearch'
+        )
 
-arg_parser.add_argument('--search_serv_port', type=int, default='22869', help='port of searching handler')
-
+arg_parser.add_argument(
+        '--search_serv_port',
+        type=int,
+        default='22869',
+        help='port of searching handler'
+    )
 
 
 if __name__ == '__main__':
@@ -20,8 +29,6 @@ if __name__ == '__main__':
     uvicorn.run(
         app, port=args.search_serv_port
     )
-
-    rec_service = RecomendationService()
 
     dispathcer = MLDispatcher()
 
