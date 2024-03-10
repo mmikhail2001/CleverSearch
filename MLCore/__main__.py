9cf3143d7590515a709f504a0cc3529cd6b54d76
Maksim Kononykhin <kononelder@gmail.com>
2024-03-10 17:15:16 +0300

    move rec service into Services folder

--- MLCore/__main__.py
+++ MLCore/__main__.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(1, './MLCore/')
 sys.path.insert(2, './MLCore/Services')
 from Services.ImageService import ImageService
+from Services.RecomendationService import RecomendationService
 
 arg_parser = ArgumentParser('MLCore', description='heart of ML in CleverSearch')
 
