bdfea87116c1938662544a338195feb221e1b434
Anastasia Dolotova <ad4019809@gmail.com>
2024-03-16 17:03:05 +0300

    ML-CLEVER-A: изменение расчета косинусной близости в ручке поиска

--- MLCore/Services/RecomendationService.py
+++ MLCore/Services/RecomendationService.py
@@ -3,15 +3,14 @@ from fastapi import FastAPI
 from pandas import DataFrame
 from pymongo import MongoClient
 from pymongo.collection import Collection
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from Processors.TextProcessor import TextProcessor
 
 app = FastAPI()
 
 def setup_search_handler(args):
     @app.get('/search')
     def search(query, file_type, user_id, dir, disk, number_of_results=5):
-        
         client = MongoClient(f'mongodb://{args.mongo_addr}:{args.mongo_port}/')
         mongo_collection = client[args.mongo_DB_name][args.mongo_collection_name]
 
@@ -22,30 +21,29 @@ def setup_search_handler(args):
                 {'status': "processed"},
             ]
         }
-        
+
         cursor = mongo_collection.find(q)
         list_cur = list(cursor)
-        
+
         df = DataFrame(list_cur)
 
-        txt_list = [list(val.values())[0] for val in df.ml_data]
-        txt_list.append(query)
+        list_embs = [list(val.values())[0] for val in df.ml_data]
 
-        vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(txt_list).todense()
+        text_processor = TextProcessor
+        query_emb = text_processor.process_query_string(query)
+        
+        dists = []
 
-        cosine = [cosine_similarity(
-            np.array(vec), np.array(X[-1])
-            ) for vec in X[:-1]]
+        for i in range(len(list_embs)):
+            for j in range(len(list_embs[i])):
+                dists.append(
+                    (i, j, cosine_similarity(list_embs[i][j], query_emb))
+                )
+        sorted_list = sorted(dists, key=lambda x: x[2], reverse=True)[:number_of_results]
+        file_keys = list(map(lambda x: x[0], sorted_list))
 
-        tmp_dict = {index: value for index, value in enumerate(cosine)}
-        sorted_dict = dict(sorted(
-            tmp_dict.items(), key=lambda item: item[1], reverse=True
-            ))
-
-        keys = list(sorted_dict.keys())[:number_of_results]
-
-        files_uuid = [{"index": k, "file_uuid": df.iloc[k]._id} for k in keys]
+        files_uuid = [{"index": k, "file_uuid": df.iloc[k]._id} for k in file_keys]
         return {"files": files_uuid}
-    
+        # return {"files": files_uuid, "sentences": sentences}
+
     return search
