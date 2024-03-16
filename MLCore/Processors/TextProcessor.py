38ab0d852a385e1fbe88deeef308d97c604bc967
Anastasia Dolotova <ad4019809@gmail.com>
2024-03-16 09:36:53 +0300

    ML-CLEVER-A: class TextProcessor realization

--- MLCore/Processors/TextProcessor.py
+++ MLCore/Processors/TextProcessor.py
@@ -1,2 +1,40 @@
+from IDataProcessor import IDataProcessor
 from TextPreprocessor import TextPreprocessor
+from transformers import AutoModel, AutoTokenizer
 
+
+class TextProcessor(IDataProcessor):
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "cointegrated/rubert-tiny"
+            )
+        self.model = AutoModel.from_pretrained("cointegrated/rubert-tiny")
+        self.model.eval()
+
+    def process(self, filename):
+        preprocessor = TextPreprocessor(filename)
+        text = preprocessor.process()
+
+        embeddings = []
+
+        for sentence in text:
+            encodes = self.tokenizer(
+                sentence,
+                return_tensors='pt',
+                padding=True)
+
+            embedding = self.model(**encodes).last_hidden_state[:, 0, :]
+            embeddings.append(embedding)
+
+        return embeddings
+
+    def process_query_string(self, query_string):
+        text_processor = TextProcessor(query_string)
+        processed_text = text_processor.process()
+        query_tokens = self.tokenizer(
+            processed_text,
+            return_tensors='pt',
+            padding=True)
+
+        query_embedding = self.model(**query_tokens).last_hidden_state[:, 0, :]
+        return query_embedding
