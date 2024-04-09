438d01453870e5cb9744c4b82839ec9700e243d5
Maksim Kononykhin <kononelder@gmail.com>
2024-04-09 10:28:10 +0300

    WIP: теперь поиск происходит в классе

--- MLCore/Services/RecomendationService.py
+++ MLCore/Services/RecomendationService.py
@@ -25,17 +25,33 @@ class SearchService():
     def __init__(self):
         pass
 
-    def search_handler(self, **kwargs):
-        params = {
-            'file_type': kwargs['file_type'],
-            'status': "processed",
-            'dir': "/",
-            'user_id': kwargs['user_id']
-        }
+    def search_handler(self, params, **kwargs):
 
         data_array = self.get_data_array(params)
 
+        text_processor = TextProcessor()
+        query_emb = [text_processor.process_query_string(kwargs['query'])]
+
         list_embs = [[obj['ml_data'][0]['Value']] for obj in data_array]
+
+        dists = []
+
+        for i in range(len(list_embs)):
+            for j in range(len(list_embs[i][0])):
+                dists.append(
+                    (i, j, cosine_similarity([list_embs[i][0][j]], query_emb))
+                )
+        logger.info(dists)
+        sorted_list = sorted(dists, key=lambda x: x[2], reverse=True)[:5]
+
+        if params['file_type'] == 'audio' or params['file_type'] == 'video':
+            return self.find_audio_files(data_array, sorted_list, params['file_type'])
+        elif params['file_type'] == 'text':
+            return self.find_text_files(data_array, sorted_list, params['file_type'])
+        elif params['file_type'] == 'img':
+            pass
+        else:
+            raise HTTPException(status_code=400, detail=f"bad file type")
 
     def get_data_array(self, params):
         response = requests.get("http://backend:8080/api/ml/files", params=params)
@@ -51,15 +67,25 @@ class SearchService():
         
         return data_array
 
+    def find_text_files(self, data_array, sorted_list, file_type):
+        result = {}
+        for elem in sorted_list:
+            if elem[0] not in result:
+                result[elem[0]] = elem[1]
+        files_uuid = [{"file_uuid": data_array[k]['id'], 'page': data_array[k]['ml_data'][1]['Value'][result[k]]} for k in list(result.keys())]
+        return {file_type: files_uuid}
 
-    def find_text_files(self):
-        pass
+    def find_audio_files(self, data_array, sorted_list, file_type):
+        result = {}
+        for elem in sorted_list:
+            if elem[0] not in result:
+                result[elem[0]] = elem[1]
+        files_uuid = [{"file_uuid": data_array[k]['id'], 'timestamp': data_array[k]['ml_data'][1]['Value'][result[k]]} for k in list(result.keys())]
+        return {file_type: files_uuid}
 
-    def find_audio_files(self):
-        pass
-    
 
 def setup_search_handler(args):
+    search_service = SearchService()
     @app.get('/search')
     def search(query, file_type, user_id, dir, disk, number_of_results=5):
         params = {
@@ -69,37 +95,5 @@ def setup_search_handler(args):
             'user_id': user_id
         }
 
-        response = requests.get("http://backend:8080/api/ml/files", params=params)
-
-        if response.status_code != 200:
-            print("Error /api/ml/files:", response.status_code)
-            raise HTTPException(status_code=500, detail=f"Error /api/ml/files: {response.status_code}, body: {response.text}")
-
-        response_data = response.json()
-        data_array = response_data.get('body', [])
-        logger.info(data_array)
-        if len(data_array) == 0:
-            raise HTTPException(status_code=400, detail=f"Files not found")
-        
-        list_embs = [[obj['ml_data'][0]['Value']] for obj in data_array]
-
-        text_processor = TextProcessor()
-        query_emb = [text_processor.process_query_string(query)]
-        
-        dists = []
-
-        for i in range(len(list_embs)):
-            for j in range(len(list_embs[i][0])):
-                dists.append(
-                    (i, j, cosine_similarity([list_embs[i][0][j]], query_emb))
-                )
-        logger.info(dists)
-        sorted_list = sorted(dists, key=lambda x: x[2], reverse=True)[:number_of_results]
-        result = {}
-        for elem in sorted_list:
-            if elem[0] not in result:
-                result[elem[0]] = elem[1]
-        files_uuid = [{"file_uuid": data_array[k]['id'], 'timestamp': data_array[k]['ml_data'][1]['Value'][result[k]]} for k in list(result.keys())]
-        return {params['file_type']: files_uuid}
-
+        return search_service.search_handler(params, query=query)
     return search
