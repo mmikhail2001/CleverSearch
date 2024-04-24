d57aa96429ae66a4ce01d212078af9d7ee1add94
mmikhail2001 <mrr.myadelets@mail.ru>
2024-04-24 16:34:31 +0300

    dev-RK3_CLEVER: ml/files без дублирования засчет наличия статуса в запросе

--- backend/internal/usecase/file/contract.go
+++ backend/internal/usecase/file/contract.go
@@ -15,6 +15,7 @@ type Repository interface {
 	PublishMessage(ctx context.Context, file file.File) error
 	GetFileByID(ctx context.Context, uuidFile string) (file.File, error)
 	GetFileByPath(ctx context.Context, path string, userID string) (file.File, error)
+	GetSharedDir(ctx context.Context, fileID string, userID string) (file.SharedDir, error)
 	GetSharedDirs(ctx context.Context, path string, userID string, accepted bool) ([]file.File, error)
 	SmartSearch(ctx context.Context, options file.FileOptions) ([]file.File, error)
 	Search(ctx context.Context, options file.FileOptions) ([]file.File, error)
@@ -26,7 +27,6 @@ type Repository interface {
 	DownloadFile(ctx context.Context, filePath string) (io.ReadCloser, error)
 	// AddUserToSharingDir(ctx context.Context, file file.File, userID string, accessType file.AccessType) error
 	IsBucketEmpty(ctx context.Context, bucketName string) (bool, error)
-	GetSharedDir(ctx context.Context, fileID string, userID string) (file.SharedDir, error)
 	InsertSharedDir(ctx context.Context, sharedDir file.SharedDir) error
 	UpdateSharedDir(ctx context.Context, sharedDir file.SharedDir) (file.SharedDir, error)
 	GetAllFiles() ([]file.File, error)
