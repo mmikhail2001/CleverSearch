b9ba8b5c6e4c738fd571600da8240c812f411373
mmikhail2001 <mrr.myadelets@mail.ru>
2024-04-30 12:12:11 +0300

    files-v2_BACK-CLEVER-12: processed возвращает mldata (fix)

--- backend/internal/delivery/file/dto.go
+++ backend/internal/delivery/file/dto.go
@@ -55,8 +55,31 @@ func (fd *FileDTO) MarshalJSON() ([]byte, error) {
 }
 
 type FileForMLDTO struct {
-	FileDTO
-	MLData interface{} `json:"ml_data"`
+	ID          string          `json:"id"`
+	Filename    string          `json:"filename"`
+	TimeCreated time.Time       `json:"time_created"`
+	UserID      string          `json:"user_id"`
+	Email       string          `json:"email"`
+	Path        string          `json:"path"`
+	Bucket      string          `json:"bucket"`
+	IsDir       bool            `json:"is_dir"`
+	FileType    file.FileType   `json:"file_type"`
+	Size        string          `json:"size"`
+	ContentType string          `json:"content_type"`
+	Extension   string          `json:"extension"`
+	Status      file.StatusType `json:"status"`
+	IsShared    bool            `json:"is_shared"`
+	ShareAccess file.AccessType `json:"share_access"`
+	ShareLink   string          `json:"share_link"`
+	Link        string          `json:"link"`
+	CloudID     string          `json:"cloud_id"`
+	CloudEmail  string          `json:"cloud_email"`
+	Disk        file.DiskType   `json:"disk"`
+	PageNumber  int             `json:"page_number"`
+	Timestart   int             `json:"timestart"`
+	Duration    time.Duration   `json:"duration"`
+	IsFav       bool            `json:"is_fav"`
+	MLData      interface{}     `json:"ml_data"`
 }
 
 type DeleteFilesDTO struct {
